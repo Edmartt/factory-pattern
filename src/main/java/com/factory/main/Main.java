0c7377e9cf844cb861d2deea8e3bf5d2a358a080
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-04 17:17:18 -0500

    Se agrega un objeto de tipo Instances que genera una instancia según el gestor de bd elegido. Este objeto debe ser añadido como argumento al metodo getDBManager para devolver la conexión al gestor de bd deseado

--- src/main/java/com/factory/main/Main.java
+++ src/main/java/com/factory/main/Main.java
@@ -1,13 +1,15 @@
 package com.factory.main;
-
-import com.factory.dbmaangers_factory.Factory;
+import com.factory.dbmaangers_factory.Instances;
+import com.factory.dbmaangers_factory.InstanceSQLite;
 import com.factory.dbmanagers.iDatabaseManager;
+import com.factory.dbmaangers_factory.Factory;
 
 public class Main {
 
     public static void main(String... args) {
         Factory factory = new Factory();
-        iDatabaseManager db_connector = factory.getDBManager(DbManagerList.SQLITE);
+        Instances managerInstance = new InstanceSQLite();
+        iDatabaseManager db_connector = factory.getDBManager(managerInstance, DbManagerList.SQLITE);
         db_connector.getConnection();
     }
 }
